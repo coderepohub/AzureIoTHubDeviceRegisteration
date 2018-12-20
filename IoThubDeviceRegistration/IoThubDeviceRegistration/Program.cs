80a26d29cbca3e72786a5953dc701acf29104c7b
Aman <amanchoudhary.com@gmail.com>
2018-12-20 15:52:30 +0530

    Added the solution to repository

---
+++ IoThubDeviceRegistration/IoThubDeviceRegistration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace IoThubDeviceRegistration
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "<IOTHUB_CONNECTIONSTRING>";
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "<YOUR_DEVICE_ID>";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# A C# Console Application to register the device in Azure AD
+A simple c# console application to register a device in Azure IoT Hub
+
+- Use this application to register a device into Azure IOT Hub 
+
+Make sure to add *Microsoft.Azure.Devices* Nuget Package in the solution
